c2c1e2ea5cbae4357befe1fbe17a6c8d7b3fd188
juan-phzy <jp.hernandez.yz@gmail.com>
2024-02-24 20:14:42 -0500

    Navbar Created

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 const page = () => {
-	return <div>page</div>;
+	return (
+		<section className="home-container">
+			Home Page
+			<div className="w-[100px] h-[100px] border-2 border-solid border-black">
+				<div className="w-[50px] h-full border-2 border-solid border-red-700">
+					blah
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default page;
---
+++ components/shared/Navbar.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const Navbar = () => {
+	return (
+		<section className="nav-container">
+			<div className="nav-menu-icon">MENU</div>
+			<div className="nav-logo">LOGO</div>
+		</section>
+	);
+};
+
+export default Navbar;
