250e89d9318a0bdf2a51408bb3c4d201c93b3115
juan-phzy <jp.hernandez.yz@gmail.com>
2024-02-25 13:59:29 -0500

    NavBar Updates

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,10 +1,37 @@
 "use client";
 
+import Image from "next/image";
+import { useState } from "react";
+
 const Navbar = () => {
+	const [menuOpen, setMenuOpen] = useState(false);
+
 	return (
 		<section className="nav-container">
-			<div className="nav-menu-icon">MENU</div>
-			<div className="nav-logo">LOGO</div>
+			<div className="nav-icon" onClick={() => setMenuOpen(!menuOpen)}>
+				<div
+					className={`nav-icon-bar w-[30px] ${
+						menuOpen && `rotate-[45deg]`
+					} transition-all`}
+				></div>
+				<div
+					className={`nav-icon-bar w-[25px] ${menuOpen && `hidden`}`}
+				></div>
+				<div
+					className={`nav-icon-bar w-[15px] ${
+						menuOpen &&
+						`w-[30px] rotate-[-45deg] absolute left-2 bottom-[21px]`
+					} transition-all`}
+				></div>
+			</div>
+			<div className="nav-logo">
+				<Image
+					className="object-contain"
+					src="/images/logo.png"
+					alt="Logo"
+					fill
+				/>
+			</div>
 		</section>
 	);
 };
