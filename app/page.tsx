a3680c44d4a598855e2608fb3ac5567ef69e8bd8
juan-phzy <jp.hernandez.yz@gmail.com>
2024-04-02 13:43:15 -0400

    Yanez Site Restart

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,10 @@
+import { HomeSlide } from "@/components/slides/HomeSlide";
 import Image from "next/image";
-import { PiArrowCircleRightThin } from "react-icons/pi";
 
 const page = () => {
 	return (
 		<section className="home-container">
-			<div className="hero-container">
-				<div className="hero-text">
-					<span>CAPTURE</span>
-					<span>YOUR</span>
-					<span>PERFECT SHOT</span>
-				</div>
-				<div className="hero-btn-container">
-					<button className="btn-trns">
-						See Services <PiArrowCircleRightThin size={30} />
-					</button>
-				</div>
-			</div>
+			<HomeSlide />
 		</section>
 	);
 };
---
+++ components/slides/HomeSlide.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import React from "react";
+import { PiArrowCircleRightThin } from "react-icons/pi";
+
+export const HomeSlide = () => {
+	return <div className="hero-container"></div>;
+};
